63f9b2d9604a8e548d074183ed1b12ca7ad0fc64
Oleh Lukash <lukasholeg02@gmail.com>
2024-12-15 00:58:18 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,11 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  // write code here
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
+
+  if (e.target === wall) {
+    spider.style.top = e.y - wall.offsetTop - spider.clientHeight / 2 + 'px';
+    spider.style.left = e.x - wall.offsetLeft - spider.clientWidth / 2 + 'px';
+  }
 });
